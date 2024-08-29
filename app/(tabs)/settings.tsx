f420d97d5047d135f38611dac79c066da05a711e
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-08-29 14:47:41 -0400

    navigation testing

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Settings</Text>
+      <Link href='/(home)/'>Home</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
